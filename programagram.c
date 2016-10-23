c94e570865ae139ef8c27792268ddc0831b17f0a
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2016-10-22 23:55:51 -0200

    fonte com o codigo do programa que implementa a ortonormalizacao utilizando o metodo de gram-schmidt

---
+++ programagram.c
@@ -0,0 +1,26 @@
+/*
+Olá!
+Eu tinha dito que queria que vocês implementassem o método de Gram-Schmidt.
+Então eis como eu o quero:
+-Entrada:
+Deve ler o arquivo texto ortogonaliza.txt que terá as sete linhas como abaixo
+//Estes são os vetores que devem ser usados
+(1,2,3,4,5)
+(1,2,3,4,4)
+(1,1,3,4,3)
+(1,1,2,4,2)
+(1,1,1,1,1)
+Fim do arquivo
+-Saída: deve me apresentar o resultado do processo no mesmo arquivo lido, logo abaixo da linha "fim do arquivo".
+Observações.
+- O arquivo pode ter, eventualmente, vetores que não formam uma base.
+  O programa deve decidir antes, se os vetores lidos são LI (se não forem, o que acontece com o programa?)
+- Eu só sei compilar em C e no linux.
+  Então seu programa deve ter instruções sobre compilação se você fugir do padrão ou precisar de algo diferente.
+- Você deve apresentar o exercício num arquivo compactado (zipado) com uma pasta contendo o código fonte do programa.
+  Tanto a pasta quanto o arquivo zipado devem conter seu número de matrícula, seu nome e último sobrenome como no exemplo
+  1049374937antonioneri.zip
+O nome do arquivo fonte deve ser programagram.c ou equivalente.
+Me pergunte neste tópico do fórum dúvidas sobre a apresentação.
+Até logo.
+*/
