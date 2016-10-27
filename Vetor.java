8515c95be2bfb6a0112a1e03183a11ca4b1e4e46
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2016-10-27 13:55:46 -0200

    sem alteracoes

--- Vetor.java
+++ Vetor.java
@@ -3,9 +3,17 @@ public class Vetor{
   public double norma;
   public double vetUnitario[];
 
+
+
+  public Vetor(int tamanho){
+    this.vet = new double[tamanho];
+    this.norma = 0;
+    this.vetUnitario = new double[tamanho];
+
+  }
   public Vetor(double v[]){
     this.vet = new double[v.length];
-    this.norma = 0;
+
     this.vetUnitario = new double[v.length];
     //System.out.println("VET");
     for(int i = 0; i < v.length; i++){
@@ -14,6 +22,9 @@ public class Vetor{
       //System.out.println(" [" + this.vet[i] + "]");
       this.vetUnitario[i] = 0;
     }
+    this.VetorUnitario();
+    this.Norma();
+    //System.out.println(this.norma);
   }
 
   // metodo que calcula a norma do vetor
@@ -24,9 +35,12 @@ public class Vetor{
     }
     tam = Math.sqrt(tam);
     this.norma = tam;
+  //  System.out.println(this.norma);
+
   }
   // metodo que calcula o vetor unitario
   public void VetorUnitario(){
+    double[] vetUnitario = new double[this.vet.length];
     for(int i = 0; i < this.vet.length; i++){
       this.vetUnitario[i] = this.vet[i] / this.norma ;
     }
